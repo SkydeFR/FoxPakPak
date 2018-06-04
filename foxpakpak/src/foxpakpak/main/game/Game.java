de98c70394080725922e92bd29b5a1400fa58ddd
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-06-04 21:07:24 +0200

    Edited Game Class
    Création de boutons et ajout d'un écouteur souris customisé

--- foxpakpak/src/foxpakpak/main/game/Game.java
+++ foxpakpak/src/foxpakpak/main/game/Game.java
@@ -10,14 +10,20 @@ import java.awt.Color;
 import java.awt.Cursor;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 import javax.swing.JOptionPane;
 
 /**
  * Jeu
  * @author jp032952
  */
-public class Game extends iut.Jeu {
-      
+public class Game extends iut.Jeu implements MouseListener, MouseMotionListener {
+    
+    /**
+     *
+     */
     public static Menus menu;
     
     /**
@@ -45,9 +51,10 @@ public class Game extends iut.Jeu {
     public Game(int width, int height, String title) {
         super(width, height, title);
         this.setLocation(5, 5); //On centre l'interface graphique
-        Game.menu = PRINCIPAL; //Menu par défaut = menu principal
+        this.ajouteEcouteurSouris(this); //On ecoute la souris
+        Game.menu = PRINCIPAL; //Menu par defaut = menu principal
     }
-
+    
     /**
      * Creation des objets en jeu
      */
@@ -61,49 +68,91 @@ public class Game extends iut.Jeu {
      * @param g : l'interface graphique
      */
     @Override
-    protected void dessinerArrierePlan(Graphics g) {       
+    protected void dessinerArrierePlan(Graphics g) {
+        /* On affiche le curseur */
+        Cursor cursor=Cursor.getDefaultCursor();
+        this.setCursor(cursor);
+        
         switch (menu) {
             case PRINCIPAL :
                 int xCoord;
                 int yCoord;
                 
-                /* On affiche le curseur */
-                Cursor cursor=Cursor.getDefaultCursor();
-                this.setCursor(cursor);
-                
-                /* Afficher background */
+                /* Afficher BACKGROUND */
                 g.setColor(Color.BLACK);
                 g.fillRect(0, 0, largeur(), hauteur());
                 
-                /* Afficher texte */
+                /* Afficher TITRE */
                 g.setColor(Color.ORANGE);
                 g.setFont(new Font("TimesRoman", Font.PLAIN, 100));
                 xCoord = largeur()/2 - 330;
                 yCoord = hauteur()/2 - 200;
                 g.drawString("TheFoxPakpak",xCoord,yCoord);
                 
-                /* Afficher boutton */
+                /* Afficher BOUTTON_01 */
                 xCoord = largeur()/2 - 200;
                 yCoord = hauteur()/2 - 100;
                 g.setColor(Color.DARK_GRAY);
                 g.draw3DRect(xCoord, yCoord, 400, 100, true);
                 g.fill3DRect(xCoord+1, yCoord+1, 400-1, 100-1, true);
+                /* --- */
+                g.setColor(new Color(0,0,75,255));
+                g.setFont(new Font("TimesRoman", Font.PLAIN, 50));
+                xCoord = largeur()/2 - 200 + 30;
+                yCoord = hauteur()/2 - 100 + 65;
+                g.drawString("Nouvelle Partie",xCoord,yCoord);
                 
-                /* Afficher boutton */
+                /* Afficher BOUTTON_02 */
                 xCoord = largeur()/2 - 200;
                 yCoord = hauteur()/2 + 50;
                 g.setColor(Color.DARK_GRAY);
                 g.draw3DRect(xCoord, yCoord, 400, 100, true);
                 g.fill3DRect(xCoord+1, yCoord+1, 400-1, 100-1, true);
+                /* --- */
+                g.setColor(new Color(0,0,75,255));
+                g.setFont(new Font("TimesRoman", Font.PLAIN, 50));
+                xCoord = largeur()/2 - 200 + 120;
+                yCoord = hauteur()/2 + 50 + 65;
+                g.drawString("Quitter",xCoord,yCoord);
                 break;
             case GAME :
-                g.setColor(Color.RED);
+                /* Afficher BACKGROUND */
+                g.setColor(Color.BLACK);
                 g.fillRect(0, 0, largeur(), hauteur());
+                break;
             default :
                 System.out.println("Type de menu non implémenté");
         }
     }
-
+    
+    /**
+     * Fonction appelle lors d'un clic souris
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        switch (e.getButton()) {
+            case MouseEvent.BUTTON1 :
+                /* Verifie si l'on a clique sur un bouton du menu */
+                boolean onButton01 = e.getX() > largeur()/2 - 200 && e.getX() < largeur()/2 - 200 + 400 &&
+                                    e.getY() > hauteur()/2 - 100 && e.getY() < hauteur()/2 - 100 + 100;
+                boolean onButton02 = e.getX() > largeur()/2 - 200 && e.getX() < largeur()/2 - 200 + 400 &&
+                                    e.getY() > hauteur()/2 + 50 && e.getY() < hauteur()/2 + 50 + 100;
+                if (onButton01) {
+                    menu = GAME; //Nouvelle partie : on affiche le jeu
+                } else if (onButton02) {
+                    System.exit(0); //Quitter : on quitte le jeu
+                }
+                break;
+            case MouseEvent.BUTTON2 :
+                //System.out.println("Click 2");
+                break;
+            case MouseEvent.BUTTON3 :
+                //System.out.println("Click 3");
+                break;
+        }
+    }
+    
     /**
      * Fonction appelee lorsque pacman n'a plus de vie
      */
@@ -136,6 +185,60 @@ public class Game extends iut.Jeu {
     @Override
     protected boolean aPerdu() {
        return false;
+    }
+    
+    /**
+     * Fonction appelle lors d'un pressement d'un bouton de la souris
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mousePressed(MouseEvent e) {  
+        
+    }
+    
+    /**
+     * Fonction appelle lors d'un relachement d'un bouton de la souris
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+    
+    /**
+     * Fonction appelle lorsque la souris entre sur l'interface graphique
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+    
+    /**
+     * Fonction appelle lorsque la souris quitte l'interface graphique
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+    
+    /**
+     * Fonction appelle lors d'un glisse-deposer de souris
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseDragged(MouseEvent e) {
+        
+    }
+    
+    /**
+     * Fonction appelle lors d'un mouvement de souris
+     * @param e : l'evenement souris
+     */
+    @Override
+    public void mouseMoved(MouseEvent e) {
+        
     }
     
 }
