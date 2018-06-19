eb7ddfea132bf55053e5d16e64b5aa348355f8de
Julien Pinto Da Fonseca <8479870+julien-pintodafonseca@users.noreply.github.com>
2018-06-19 23:47:40 +0200

    Revert deletion
    This partially reverts commit 97ff67341c2ea63356c2f380742c9ba08c96291f.

---
+++ foxpakpak/src/foxpakpak/main/Game/Level/Cases/Case.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foxpakpak.main.Game.Level.Cases;
+
+import iut.Jeu;
+import iut.Objet;
+import iut.ObjetTouchable;
+
+/**
+ * Case
+ * @author jp032952
+ */
+public abstract class Case extends ObjetTouchable {
+    
+    private boolean franchissable;
+    
+    /**
+     * Constructeur
+     * @param x Coordonnée x de la case
+     * @param y Coordonnée y de la case
+     */
+    public Case(Jeu g, String nom, int x, int y) {
+        super(g, nom, x, y);
+    }
+    
+    public boolean isFranchissable() {
+        return franchissable;
+    }
+    
+    public void setFranchissable(boolean bool) {
+        franchissable = bool;
+    }
+    
+    @Override
+    public void evoluer(long dt) {
+        
+    }
+    
+    @Override
+    public void effetCollision(Objet objet) {
+        
+    }
+}
---
+++ foxpakpak/src/foxpakpak/main/Game/Level/Cases/Sol.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package foxpakpak.main.Game.Level.Cases;
+
+import iut.Jeu;
+
+/**
+ * Sol
+ * @author Ange Pagel
+ */
+public class Sol extends Case {
+    
+    public Sol(Jeu g, String nom, int x, int y) {
+        super(g, nom, x, y);
+    }
+    
+    @Override
+    public String getTypeObjet() {
+        return "SOL";
+    }
+    
+}
